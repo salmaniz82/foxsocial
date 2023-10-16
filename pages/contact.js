45bc931aaa45da9518e1e6155843cab4fa3d88ab
salmaniz82 <salmaniz.82@gmail.com>
2023-10-17 03:25:05 +0500

    contact form float labels state management bind input

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,45 @@
 import {motion} from 'framer-motion';
 import Head from 'next/head';
 
+import css from '../styles/contact.module.css';
+import { useEffect, useState } from 'react';
+
 
 
 function Contact(){
 
-    return (
+      const [formPayload, setFormPayload] = useState({
+        firstname : "",
+        lastname : "",
+        email: "", 
+        subject: "",
+        message : ""
+      });
 
+
+     const handleChange = (e) => {
+
+
+        const {name, value} = e.target;
+
+        console.log(name, value);
         
+
+        setFormPayload(oldValue => ({
+
+            ...oldValue, [name]: value
+
+        }))
+
+      };
+
+
+   
+
+      
+
+    return (
+       
         <>        
 
         <Head>
@@ -28,11 +60,90 @@ function Contact(){
         <h2 className="page-title font-AlegreyaSans">Contact</h2>
         
         <div className="page-details">
-            <p>Pellentesque ac pulvinar ante. Cras porttitor lorem ex, vitae iaculis ligula vehicula sed. Vivamus mollis molestie elit, eu mattis lacus sollicitudin at. Nam id velit est. Quisque id nulla et purus vestibulum ornare. Morbi gravida ipsum felis, a scelerisque velit varius a. Integer ut ipsum tortor. Donec in sapien nec nibh suscipit euismod.</p>
 
-             <p>Duis suscipit vulputate hendrerit. Vestibulum purus lorem, tristique quis commodo ut, tincidunt consequat sem. Aliquam at quam ac magna lacinia faucibus. In nec convallis tortor. Sed ut placerat felis. Nullam vulputate aliquam diam vehicula finibus. Fusce egestas gravida venenatis. Quisque faucibus sapien enim, in elementum elit facilisis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>   
+            <div className="contact_formWrapper">
 
-             <p>Fusce consectetur quis libero ac tincidunt. Donec efficitur nulla non quam ullamcorper volutpat. Phasellus semper gravida libero et laoreet. Aenean viverra nulla eget risus tempor consequat. Phasellus auctor nibh nec felis condimentum placerat. Quisque consectetur, lectus sit amet malesuada luctus, arcu massa vestibulum mi, ac rhoncus ligula nisi a ante. Donec lacinia massa non vulputate consectetur.</p>
+                <div className="text">This is where text will go</div>
+
+                <div className="contact__form">
+
+                    
+                    <form action="#" method="POST" className={css.floatable}>
+
+                        <div className={css['dfx-2Col']}>
+                        <div className={css['field_wrap']}>
+                            <input 
+                                type="text" 
+                                name="firstname" 
+                                className={`${css.floadField}  ${!formPayload.firstname.length ? (css['empty']) : (css['not-empty'])}`} 
+                                id="firstname" 
+                                value={formPayload.firstname}
+                                onChange={handleChange}
+                                
+                                />
+                            <label htmlFor="firstname" className={css['float-label']}>First name: </label>
+                        </div>
+
+                        <div className={css['field_wrap']}>
+                            <input type="text" 
+                                name="lastname" 
+                                className={`${css.floadField}  ${!formPayload.firstname.length ? (css['empty']) : (css['not-empty'])}`} 
+                                id="lastname" 
+                                value={formPayload.lastname}
+                                onChange={handleChange}
+                                />
+                            <label htmlFor="lastname" className={css['float-label']}>Last name:</label>
+                        </div>
+
+                        </div>
+
+
+                        <div className={css['field_wrap']}>
+                            <input 
+                                type="email" 
+                                name="email" 
+                                className={`${css.floadField}  ${!formPayload.firstname.length ? (css['empty']) : (css['not-empty'])}`} 
+                                id="email"
+                                value={formPayload.email}
+                                onChange={handleChange}
+
+                                />
+                            <label htmlFor="email" className={css['float-label']}>Email</label>
+                        </div>
+
+                        <div className={css['field_wrap']}>
+                            <input 
+                                type="text" 
+                                name="subject" 
+                                className={`${css.floadField}  ${!formPayload.firstname.length ? (css['empty']) : (css['not-empty'])}`} 
+                                id="subject"
+                                value={formPayload.subject}
+                                onChange={handleChange}
+                                />
+                            <label htmlFor="subject" className={css['float-label']}>Subject</label>
+                        </div>
+
+                        <div className={css['field_wrap']}>
+                            <textarea className={css.floadField} id="textarea" name="message" onChange={handleChange}>
+
+                            {formPayload.message}
+
+                            </textarea>
+                            <label htmlFor="textarea" className={css['float-label']}>Message</label>
+                        </div>
+
+
+                        <div className={css.submit__CTA}>
+                            <button type="submit">SUBMIT</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+            
         </div>
 
 
